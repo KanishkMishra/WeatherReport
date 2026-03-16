cb428696b6b48a8471a46d726fcd63dfa623435d
Kanishk Mishra <Kmishra7@uwo.ca>
2026-03-16 14:03:44 -0400

    Increase data available in the weather map with customizability

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import { MapContainer, TileLayer } from "react-leaflet";
+import { LayersControl } from "react-leaflet";
 import SearchBar from "../SearchBar";
 
 
@@ -20,27 +20,68 @@ function MapPage({
                 city={city}
                 setCity={setCity}
                 fetchWeather={fetchWeather}
-                title='Precipitation Map'
+                title='Weather Map'
                 suggestions={suggestions}
             />
-            {freeAPIKey && weather && 
+            {freeAPIKey && weather && (() => {
+                const savedLocation =  JSON.parse(localStorage.getItem("location"));
+                const { BaseLayer, Overlay } = LayersControl;
+                return (
                 <div className="WeatherData">
+                    <h2>{[savedLocation.name, savedLocation.state, savedLocation.country].filter(Boolean).join(",")}</h2>
+                    
                     <MapContainer 
                         center={[weather.coord.lat, weather.coord.lon]} 
-                        zoom={6} 
-                        style={{ height: "70vh", width: "100%"}}
+                        zoom={10} 
+                        style={{ height: "55vh", width: "90%"}}
                     >
-                        <TileLayer
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            attribution="© OpenStreetMap contributors"
-                        />
-                        <TileLayer
-                            url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${freeAPIKey}`}
-                            attribution='Weather data &copy; OpenWeatherMap'
-                        />
+                        <LayersControl position="topright">
+                            <BaseLayer checked name="Map">
+                                <TileLayer
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    attribution="© OpenStreetMap contributors"
+                                />
+                            </BaseLayer>
+
+                            <Overlay name="Precipitation">
+                                <TileLayer
+                                    url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${freeAPIKey}`}
+                                    attribution='Weather data &copy; OpenWeatherMap'
+                                />
+                            </Overlay>
+
+                            <Overlay name="Clouds">
+                                <TileLayer
+                                    url={`https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${freeAPIKey}`}
+                                    attribution='Weather data &copy; OpenWeatherMap'
+                                />
+                            </Overlay> 
+
+                            <Overlay name="Temperature">
+                                <TileLayer
+                                    url={`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${freeAPIKey}`}
+                                    attribution='Weather data &copy; OpenWeatherMap'
+                                />
+                            </Overlay>
+
+                            <Overlay name="Wind">
+                                <TileLayer
+                                    url={`https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=${freeAPIKey}`}
+                                    attribution='Weather data &copy; OpenWeatherMap'
+                                />
+                            </Overlay>
+
+                            <Overlay name="Pressure">
+                                <TileLayer
+                                    url={`https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=${freeAPIKey}`}
+                                    attribution='Weather data &copy; OpenWeatherMap'
+                                />
+                            </Overlay>
+
+                        </LayersControl>
                     </MapContainer>
                 </div>
-            }
+            )})()}
         </div>
     );
 }
